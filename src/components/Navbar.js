2f68fe873b929b58e31f8c2186ee8f787b539a85
MOHIT SAHARAN <mohitsaharan@MOHITs-MacBook-Air.local>
2024-08-05 00:52:41 +0530

    Solved navBar Problem

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -22,8 +22,8 @@ export default function Navbar() {
         <button
           className="navbar-toggler"
           type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
           aria-controls="navbarNav"
           aria-expanded="false"
           aria-label="Toggle navigation"
